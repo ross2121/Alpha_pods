342fa8b774c4e82531a4a0c64f031d3c3b70d9f8
Youval Singh <youvalsingh40@gmail.com>
2025-10-17 22:24:02 +0530

    impl swap

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { getquote } from "./services/jupiter_swap"
 import express, { json } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app=express();
 app.use(json);
 const main=async()=>{
--- src/services/jupiter_swap.ts
+++ src/services/jupiter_swap.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
-
+const URL="https://lite-api.jup.ag/ultra/v1";
 export const getquote = async (basemint: string, quotemint: string, amount: number) => {
   //curl --request GET \
   //   --url 'https://lite-api.jup.ag/ultra/v1/order?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=1'
-  const url = `https://lite-api.jup.ag/ultra/v1/order?inputMint=${basemint}&outputMint=${quotemint}&amount=${amount}`;
-  
+  const url = `${URL}/order?inputMint=${basemint}&outputMint=${quotemint}&amount=${amount}`;  
   try {
     const response = await axios.get(url);
     console.log("Response:", response.data);
@@ -14,4 +13,17 @@ export const getquote = async (basemint: string, quotemint: string, amount: numb
     throw error;
   }
 };
+export const swap=async(requestId:String,signature:String)=>{
+    const url=`${URL}/execute`
+    try{
+        const response=await axios.post(url,{
+            signedTransaction:signature,
+            requestId:requestId
+        });
+        console.log(response.data);
+    }catch(error){
+        console.log("Error executing order",error);
+        throw error;
+    }
+}
 
